7f27de52bb2c6b12b2bd9444578c5a1f84612ed8
Breno Eduardo <brenoeduardo.dev@gmail.com>
2024-11-07 14:33:38 -0300

    Conexão estabelecida entre o banco de dados. Tabelas criadas

---
+++ conexao.php
@@ -0,0 +1,18 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "meu_database";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+} echo "Conexão bem sucedida!";
+?>
